86da88d1d24f9a2a597d2b8cdf0dc4457c5cc280
Nguyen Viet <nguyenviet02.dev@gmail.com>
2025-01-08 16:18:38 +0700

    add: background in bottom section

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,22 @@ import PaymentSovereignty from "@components/PaymentSovereignty";
 import Reference from "@components/Reference";
 import MainLayout from "@layouts/MainLayout";
 
+import PolkadotAsset from "@images/polkadot-asset.png";
+
 export default function App() {
   return (
     <MainLayout>
       <Hero />
       <PaymentSovereignty />
-      <OurPartner />
-      <Reference />
+      <div className="relative flex h-fit w-full flex-col gap-[5.375rem]">
+        <img
+          className="absolute size-full object-cover object-left-top -z-[1]"
+          src={PolkadotAsset}
+          alt="XssetHub"
+        />
+        <OurPartner />
+        <Reference />
+      </div>
     </MainLayout>
   );
 }
